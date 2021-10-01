0a2dc5b08e4ce9a5f222244a16097adcc66a00f0
Sheri Warren <swarren35@yahoo.com>
2021-10-01 18:22:19 -0400

    hard code time blocks in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,17 @@
-var rightNow = moment().format("MMMM Do, YYYY");
+var rightNow = moment().format("MMMM Do, YYYY, h:mm:ss A");
 console.log(rightNow);
 $("#currentDay").text(rightNow);
 
 var timeNow = moment().hour();
 console.log(timeNow);
 const scheduleRowContainer =document.querySelector("#scheduleRow");
-const timesContainerEl = document.querySelector("#timesContainer");
-const taskContainerEl = document.querySelector("#taskContainer");
-const saveButtonContainerEl = document.querySelector("#saveContainer");
-var taskDisplayEl;
-var tasks = [];
-var newTask;
-var times = ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
-hourId = 9;
 
 
 
 
 
-var createCalendar = function() {
-    for (var i = 0; i < times.length; i++) {
-        
-        //set times
-        var hourSlotEl = document.createElement("p");
-        hourSlotEl.setAttribute("class", "textarea time-block hour");
-        hourSlotEl.textContent = times[i];
-        scheduleRowContainer.appendChild(timesContainerEl);
-        timesContainerEl.appendChild(hourSlotEl);
-        console.log(times[i]);
-        
-    
-        //set area for tasks
-        taskDisplayEl = document.createElement("textarea");
-        taskDisplayEl.textContent = "Enter task here";
-        taskDisplayEl.setAttribute("id", i + 9);
-        taskDisplayEl.setAttribute("name", "taskInput");
-        taskDisplayEl.setAttribute("class", "textarea taskBlock");
-        taskContainerEl.appendChild(taskDisplayEl);
-        scheduleRowContainer.appendChild(taskContainerEl);
-    
-        //create button
-        var saveButton = document.createElement("button")
-        saveButton.setAttribute("class", "saveBtn textarea");
-        saveButton.setAttribute("data-timeId", i + 9);
-        saveButton.textContent = "Save Me";
-        //iconEl = document.createElement("span");
-        //iconEl = setAttribute("class", "oi oi-file");
-        //saveButton.appendChild(iconEl);
-        saveButtonContainerEl.appendChild(saveButton);
-        scheduleRowContainer.appendChild(saveButtonContainerEl);
-       
-    }
-    };
+
+
 
 
 
@@ -108,7 +68,8 @@ var addTask = function(event) {
 
 
 
-saveButtonContainerEl.addEventListener("click", saveButtonHandler);
-taskContainerEl.addEventListener("click", addTask);
-createCalendar();
+//saveButtonContainerEl.addEventListener("click", saveButtonHandler);
+//taskContainerEl.addEventListener("click", addTask);
+
 checkTime();
+
